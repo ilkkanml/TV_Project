967362363b80da17bafead3c64ad274e0b0374ab
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-14 17:03:18 -0400

    Add Compose navigation host

---
+++ app/src/main/java/com/nexora/tv/navigation/NexoraNavHost.kt
@@ -0,0 +1,38 @@
+package com.nexora.tv.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.nexora.tv.ui.screens.HomeScreen
+import com.nexora.tv.ui.screens.LoginScreen
+import com.nexora.tv.ui.screens.PlayerScreen
+import com.nexora.tv.ui.screens.SplashScreen
+
+@Composable
+fun NexoraNavHost() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = AppDestinations.Splash.route
+    ) {
+
+        composable(AppDestinations.Splash.route) {
+            SplashScreen(navController)
+        }
+
+        composable(AppDestinations.Login.route) {
+            LoginScreen(navController)
+        }
+
+        composable(AppDestinations.Home.route) {
+            HomeScreen(navController)
+        }
+
+        composable(AppDestinations.Player.route) {
+            PlayerScreen()
+        }
+    }
+}
