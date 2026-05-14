45be775986af802bac3bf8e162a1171715f9ca2f
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-14 17:04:29 -0400

    Upgrade MainActivity to full navigation runtime

--- app/src/main/java/com/nexora/tv/MainActivity.kt
+++ app/src/main/java/com/nexora/tv/MainActivity.kt
@@ -3,14 +3,8 @@ package com.nexora.tv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import com.nexora.tv.navigation.NexoraNavHost
+import com.nexora.tv.ui.theme.NexoraTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -18,17 +12,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "NEXORA TV",
-                    color = Color.Cyan,
-                    fontSize = 42.sp
-                )
+            NexoraTheme {
+                NexoraNavHost()
             }
         }
     }
