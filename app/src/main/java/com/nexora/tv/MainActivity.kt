0c37fe5549f58c7fbd793e99a62ec1f24a711e36
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-14 17:00:40 -0400

    Add MainActivity runtime shell

---
+++ app/src/main/java/com/nexora/tv/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.nexora.tv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "NEXORA TV",
+                    color = Color.Cyan,
+                    fontSize = 42.sp
+                )
+            }
+        }
+    }
+}
