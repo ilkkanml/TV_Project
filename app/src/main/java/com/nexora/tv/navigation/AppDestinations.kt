237fe5b916016657d0ac45bfd3ace22dbbd613b6
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-15 20:32:46 -0400

    Add activation route destination

--- app/src/main/java/com/nexora/tv/navigation/AppDestinations.kt
+++ app/src/main/java/com/nexora/tv/navigation/AppDestinations.kt
@@ -3,6 +3,7 @@ package com.nexora.tv.navigation
 sealed class AppDestinations(val route: String) {
     data object Splash : AppDestinations("splash")
     data object Login : AppDestinations("login")
+    data object Activation : AppDestinations("activation")
     data object Home : AppDestinations("home")
     data object Player : AppDestinations("player")
 }
