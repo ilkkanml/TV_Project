371ed968259afb0ee6b1fa90e68a46f9de7f1196
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-17 15:13:35 -0400

    M8: mask login password field

--- app/src/main/java/com/nexora/tv/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/nexora/tv/ui/screens/LoginScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.nexora.tv.navigation.AppDestinations
@@ -56,6 +57,7 @@ fun LoginScreen(navController: NavController) {
             value = password,
             onValueChange = { password = it },
             label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.width(420.dp)
         )
 
