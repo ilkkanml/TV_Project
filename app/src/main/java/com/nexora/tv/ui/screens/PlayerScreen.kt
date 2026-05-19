d75e544598f436f2ff177aba7eb4ae9800c13c97
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-19 10:08:20 -0500

    Fix player overlay BoxScope alignment

--- app/src/main/java/com/nexora/tv/ui/screens/PlayerScreen.kt
+++ app/src/main/java/com/nexora/tv/ui/screens/PlayerScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -149,7 +150,7 @@ private fun CenterPlayerState() {
 }
 
 @Composable
-private fun BottomOverlay() {
+private fun BoxScope.BottomOverlay() {
     Column(
         modifier = Modifier
             .align(Alignment.BottomStart)
