b777656eeecf9bbd0c123443fb1799dd8e2cb169
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-19 01:17:53 -0500

    Enable immersive fullscreen for Nexora TV app

--- app/src/main/java/com/nexora/tv/MainActivity.kt
+++ app/src/main/java/com/nexora/tv/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nexora.tv
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.nexora.tv.navigation.NexoraNavHost
@@ -10,6 +11,14 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
         setContent {
             NexoraTheme {
