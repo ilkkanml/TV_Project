38d20d4e7b05efe39cf12c5743f8b1b0b2879460
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-20 14:56:23 -0500

    Add language and profiles destinations

--- app/src/main/java/com/nexora/tv/navigation/AppDestinations.kt
+++ app/src/main/java/com/nexora/tv/navigation/AppDestinations.kt
@@ -2,8 +2,10 @@ package com.nexora.tv.navigation
 
 sealed class AppDestinations(val route: String) {
     data object Splash : AppDestinations("splash")
+    data object Language : AppDestinations("language")
     data object Login : AppDestinations("login")
     data object Activation : AppDestinations("activation")
+    data object Profiles : AppDestinations("profiles")
     data object Home : AppDestinations("home")
     data object Detail : AppDestinations("detail/{contentId}") {
         fun createRoute(contentId: String): String = "detail/$contentId"
