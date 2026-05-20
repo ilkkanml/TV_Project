a961cac9c42deb8ed54ad92758a487fff086c815
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-20 14:57:04 -0500

    Wire language profiles nav

--- app/src/main/java/com/nexora/tv/navigation/NexoraNavHost.kt
+++ app/src/main/java/com/nexora/tv/navigation/NexoraNavHost.kt
@@ -7,48 +7,28 @@ import androidx.navigation.compose.rememberNavController
 import com.nexora.tv.ui.screens.ContentDetailScreen
 import com.nexora.tv.ui.screens.DeviceActivationScreen
 import com.nexora.tv.ui.screens.HomeScreen
+import com.nexora.tv.ui.screens.LanguageSelectionScreen
 import com.nexora.tv.ui.screens.LoginScreen
 import com.nexora.tv.ui.screens.PlayerScreen
 import com.nexora.tv.ui.screens.PlaylistProfileScreen
 import com.nexora.tv.ui.screens.SplashScreen
+import com.nexora.tv.ui.screens.UserProfilesScreen
 
 @Composable
 fun NexoraNavHost() {
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = AppDestinations.Splash.route
-    ) {
-        composable(AppDestinations.Splash.route) {
-            SplashScreen(navController)
-        }
-
-        composable(AppDestinations.Login.route) {
-            LoginScreen(navController)
-        }
-
-        composable(AppDestinations.Activation.route) {
-            DeviceActivationScreen(navController)
-        }
-
-        composable(AppDestinations.Home.route) {
-            HomeScreen(navController)
-        }
-
+    NavHost(navController = navController, startDestination = AppDestinations.Splash.route) {
+        composable(AppDestinations.Splash.route) { SplashScreen(navController) }
+        composable(AppDestinations.Language.route) { LanguageSelectionScreen(navController) }
+        composable(AppDestinations.Login.route) { LoginScreen(navController) }
+        composable(AppDestinations.Activation.route) { DeviceActivationScreen(navController) }
+        composable(AppDestinations.Profiles.route) { UserProfilesScreen(navController) }
+        composable(AppDestinations.Home.route) { HomeScreen(navController) }
         composable(AppDestinations.Detail.route) { backStackEntry ->
-            ContentDetailScreen(
-                navController = navController,
-                contentId = backStackEntry.arguments?.getString("contentId")
-            )
+            ContentDetailScreen(navController = navController, contentId = backStackEntry.arguments?.getString("contentId"))
         }
-
-        composable(AppDestinations.Player.route) {
-            PlayerScreen(navController)
-        }
-
-        composable(AppDestinations.PlaylistProfile.route) {
-            PlaylistProfileScreen(navController)
-        }
+        composable(AppDestinations.Player.route) { PlayerScreen(navController) }
+        composable(AppDestinations.PlaylistProfile.route) { PlaylistProfileScreen(navController) }
     }
 }
