c961e8a4ca3836a068a65ccac49b1810224e4d84
ilkkan <99904094+ilkkanml@users.noreply.github.com>
2026-05-20 22:09:54 -0500

    Add release constants for NexoraTV

---
+++ app/src/main/java/com/nexora/tv/data/release/ReleaseConstants.kt
@@ -0,0 +1,16 @@
+package com.nexora.tv.data.release
+
+import com.nexora.tv.BuildConfig
+
+object ReleaseConstants {
+    const val APP_NAME = "NexoraTV"
+    const val APK_FILE_NAME = "nexoratv.apk"
+    const val APK_DOWNLOAD_URL = "https://www.thenightssecret.com/dl/nexoratv.apk"
+    const val DEVICE_INSTALL_ENDPOINT = "https://www.thenightssecret.com/api/devices/install/index.php"
+
+    val appVersionName: String
+        get() = BuildConfig.VERSION_NAME.ifBlank { "0.1.0" }
+
+    val appVersionCode: Int
+        get() = BuildConfig.VERSION_CODE
+}
